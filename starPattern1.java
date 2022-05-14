68252ff6c5d017e93e3fbacb30da7eb4838533af
niladri26 <105287219+niladri26@users.noreply.github.com>
2022-05-14 09:23:14 +0530

    doing my first upload

---
+++ starPattern1.java
@@ -0,0 +1,20 @@
+package ifelsedemo;
+public class starPattern1 {
+	public static void main(String[] args) {
+		/* apna school
+		 *   * * * * *
+		 *   * * * * *
+		 *   * * * * *
+		 *   * * * * *
+		 */
+		for (int i=1;i<=4;i++){
+			
+			for (int j=1;j<=5;j++){
+				System.out.print("* "); // to print * in single line
+			}
+			System.out.println(""); // for printing next line
+		}
+
+	}
+
+}
